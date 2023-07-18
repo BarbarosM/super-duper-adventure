24fe4b6fcd8d1d08edf80a8f20ef878600eceb99
BarbarosM <43379143+BarbarosM@users.noreply.github.com>
2023-07-18 18:36:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,34 +1,34 @@
-//create web server
+// create web server
 var express = require('express');
 var app = express();
-//create server
+// create server
 var server = require('http').createServer(app);
-//create socket
+// create socket
 var io = require('socket.io')(server);
 
-//listen to port 3000
-server.listen(3000);
+// listen to port 3000
+server.listen(3008);
 
-//use ejs
+// use ejs
 app.set('view engine', 'ejs');
 
-//use static file
+// use static file
 app.use(express.static('./public'));
 
-//create array comments
+// create array comments
 var comments = ['hello', 'world'];
 
-//render index.ejs
+// render index.ejs
 app.get('/', function(req, res){
 	res.render('index');
 });
 
-//socket
+// socket
 io.on('connection', function(socket){
-	//send comments to client
+	// send comments to client
 	socket.emit('send comments', comments);
 
-	//receive comments from client
+	// receive comments from client
 	socket.on('receive comments', function(data){
 		comments.push(data);
 		socket.broadcast.emit('send comments', comments);
